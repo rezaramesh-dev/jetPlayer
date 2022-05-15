533edde0d427a562abe9573206be90b98a095b91
Reza Ramesh <rezaramesh@icloud.com>
2022-05-15 12:09:23 +0430

    Add Permission for Manage Files Access

--- app/src/main/java/app/videoplayer/kotlin/Adapters/VideoAdapter.kt
+++ app/src/main/java/app/videoplayer/kotlin/Adapters/VideoAdapter.kt
@@ -3,6 +3,11 @@ package app.videoplayer.kotlin.Adapters
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.Settings
+import android.text.SpannableStringBuilder
 import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -12,11 +17,13 @@ import app.videoplayer.kotlin.Models.Video
 import app.videoplayer.kotlin.R
 import app.videoplayer.kotlin.View.Activitys.MainActivity
 import app.videoplayer.kotlin.View.Activitys.PlayerActivity
+import app.videoplayer.kotlin.databinding.RenameFieldBinding
 import app.videoplayer.kotlin.databinding.VideoMoreFeaturesBinding
 import app.videoplayer.kotlin.databinding.VideoViewBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.net.URI
 
 class VideoAdapter(
     private val context: Context,
@@ -73,6 +80,25 @@ class VideoAdapter(
             val bindingMF = VideoMoreFeaturesBinding.bind(customDialog)
             val dialog = MaterialAlertDialogBuilder(context).setView(customDialog).create()
             dialog.show()
+
+            bindingMF.renameBtn.setOnClickListener {
+                requestPermissionR()
+                dialog.dismiss()
+                val customDialogRF = LayoutInflater.from(context)
+                    .inflate(R.layout.rename_field, holder.root, false)
+                val bindingRF = RenameFieldBinding.bind(customDialogRF)
+                val dialogRF = MaterialAlertDialogBuilder(context).setView(customDialogRF)
+                    .setCancelable(false)
+                    .setPositiveButton("Rename") { self, _ ->
+                        self.dismiss()
+                    }
+                    .setNegativeButton("Cancel") { self, _ ->
+                        self.dismiss()
+                    }
+                    .create()
+                dialogRF.show()
+                bindingRF.renameField.text = SpannableStringBuilder.valueOf(videoList[position].title)
+            }
             return@setOnLongClickListener true
         }
     }
@@ -93,6 +119,17 @@ class VideoAdapter(
         videoList = ArrayList()
         videoList.addAll(searchList)
         notifyDataSetChanged()
+    }
+
+    private fun requestPermissionR() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                intent.addCategory("android.intent.category.DEFAULT")
+                intent.data = Uri.parse("package:${context.applicationContext.packageName}")
+                ContextCompat.startActivity(context, intent, null)
+            }
+        }
     }
 
 }
